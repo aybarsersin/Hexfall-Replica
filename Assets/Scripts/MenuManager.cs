eb25ff03c158e971fe575a364ed5cb069822baf0
aybarsersin <aybarsersin@gmail.com>
2020-07-13 15:02:02 +0300

    update

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ChangeScene(string sceneTitle)
+    {
+        SceneManager.LoadScene(sceneTitle);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
