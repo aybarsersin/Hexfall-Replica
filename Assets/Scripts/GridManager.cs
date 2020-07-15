92a0ee26a9d148cadc47426830928ad12d93ed20
aybarsersin <aybarsersin@gmail.com>
2020-07-15 21:09:30 +0300

    update
    a days work, but the work is never done.
    
    HexagonTile vs continued

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
+public class GridManager : ConstantClass
 {
     public static GridManager instance = null;
 
@@ -20,17 +20,189 @@ public class GridManager : MonoBehaviour
     private bool gameOver;
 
     private Vector2 selectedCoordinates;
-    private 
+    private HexagonTile selectedHexagonTile;
+    private List<List<HexagonTile>> gameGrid;
+    private List<HexagonTile> selectedTrio;
+    private List<HexagonTile> bombHexagons;
+    private List<Color> colourList;
 
-    void Start()
+    private bool hexagonRotateStatus;
+    private bool bombHexagonExplodeStatus;
+    private bool hexagonCreationStatus;
+    private bool gameStartStatus;
+
+    public void SettingGridWidth(int width)
     {
-        
+        gridWidth = width;
+    }
+    public void SettingGridHeight(int height)
+    {
+        gridHeight = height;
+    }
+    public void SettingColourBlindMode(bool mode)
+    {
+        colourBlindMode = mode;
+    }
+    public void SettingColourList(List<Color> colors)
+    {
+        colourList = colors;
+    }
+    public void SettingBombsAway()
+    {
+        bombsAway = true;
+    }
+    public int GettingGridWidth()
+    {
+        return gridWidth;
+    }
+    public int GettingGridHeight()
+    {
+        return gridHeight;
+    }
+    public bool GettingColourBlindMode()
+    {
+        return colourBlindMode;
+    }
+    public HexagonTile GettingSelectedHexagonTile()
+    {
+        return selectedHexagonTile;
     }
 
 
-
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void Start()
+    {
+        gameOver = false;
+        bombsAway = false;
+        hexagonRotateStatus = false;
+        hexagonCreationStatus = false;
+        bombHexagonExplodeStatus = false;
+        bombHexagons = new List<HexagonTile>();
+        selectedTrio = new List<HexagonTile>();
+        gameGrid = new List<List<HexagonTile>>();
+    }
+
+    private IEnumerator CreateHexagons(List<int> columns, List<List<Color>> colourSeed = null)
+    {
+        Vector3 initialPosition;
+        float coordinateX;
+        float coordinateY;
+        bool columnStatus;
+        float startCoordinateX = CoordinateXOfFirstColumn();
+
+        hexagonCreationStatus = true;
+
+        foreach (int n in columns)
+        {
+            columnStatus = OnColumn(n);
+            coordinateX = startCoordinateX + (_horizontalHexagonDistance * n);
+            coordinateY = (_verticalHexagonDistance * gameGrid[n].Count * _double) + _verticalGridOffset + (columnStatus ? _verticalHexagonDistance : _zero);
+
+            initialPosition = new Vector3(coordinateX, coordinateY, _zero);
+
+            GameObject newGameObject = Instantiate(hexagonPrefab,_hexagonInitialPosition, Quaternion.identity, hexagonParent.transform);
+
+            HexagonTile newHexagonTile = newGameObject.GetComponent<HexagonTile>();
+            yield return new WaitForSeconds(_hexagonSendDelay);
+
+            if (bombsAway)
+            {
+                newHexagonTile.SetBombHexagon();
+                bombHexagons.Add(newHexagonTile);
+                bombsAway = false;
+            }
+
+            if (colourSeed == null)
+            {
+                newHexagonTile.SettingHexagonColour(colourList[(int)(Random.value * _seed) % colourList.Count]);
+            }
+            else
+            {
+                newHexagonTile.SettingHexagonColour(colourSeed[n][gameGrid[n].Count]);
+            }
+
+            newHexagonTile.ChangeHexagonGridPosition(new Vector2(n, gameGrid[n].Count));
+            newHexagonTile.AlterWorldPosition(initialPosition);
+            gameGrid[n].Add(newHexagonTile);
+        }
+
+        hexagonCreationStatus = false;
+    }
+
+    public void InitializationOfGameGrid()
+    {
+        List<int> minusCells = new List<int>();
+
+        for (int n = 0; n < GettingGridWidth(); n++)
+        {
+            for (int m = 0; m < GettingGridHeight(); m++)
+            {
+                gameGrid.Add(new List<HexagonTile>());
+            }
+        }
+
+        StartCoroutine(CreateHexagons(minusCells,col))// burdasın balım
+    }
+
+    public bool OnColumn(int n)
+    {
+        int middleColumnIndex = GettingGridWidth() / _half;
+        return (middleColumnIndex % 2 == n % 2);
+    }
+
+    public bool ReadyForInput() // burayı kontrol edicez input almazsa.
+    {
+        if (hexagonCreationStatus == false && hexagonRotateStatus == false && bombHexagonExplodeStatus == false && gameOver == false)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private float CoordinateXOfFirstColumn()
+    {
+        return gridWidth / _half * -_horizontalHexagonDistance;
+    }
+
+    private bool IsPositionGameGrid(Vector2 position)
+    {
+        if (position.x >= _zero && position.y >= _zero && position.x < GettingGridWidth() && position.y < GettingGridWidth())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void PrintGameGrid() // gerekli mi değil mi ?
+    {
+        string map = "";
+
+
+        for (int i = GettingGridHeight() - 1; i >= 0; --i)
+        {
+            for (int j = 0; j < GettingGridWidth(); ++j)
+            {
+                if (gameGrid[j][i] == null)
+                    map += "0 - ";
+                else
+                    map += "1 - ";
+            }
+
+            map += "\n";
+        }
+
+        print(map);
     }
 }
--- Assets/Scripts/HexagonTile.cs
+++ Assets/Scripts/HexagonTile.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class HexagonTile : ConstantClass
 {
     GridManager GridManagerObject;
-    public int x;
-    public int y;
+    public int coordinateX;
+    public int coordinateY;
     public Color colour;
     public Vector2 linearInterpolationCoordinate;
     public bool linearInterpolation;
     public Vector2 velocity;
-    public bool bomb; //unused?
-    private int bombTimer;
+    public bool bombHexagon; //unused?
+    private int bombHexagonTimer;
     private TextMesh text;
 
     public struct NeighbouringHexagons
@@ -23,6 +23,48 @@ public class HexagonTile : ConstantClass
         public Vector2 downLeft;
         public Vector2 upRight;
         public Vector2 downRight;
+    }
+
+    public void SettingHexagonX(int value)
+    {
+        coordinateX = value;
+    }
+
+    public void SettingHexagonY(int value)
+    {
+        coordinateY = value;
+    }
+
+    public void SettingHexagonColour(Color newColour)
+    {
+        GetComponent<SpriteRenderer>().color = newColour;
+        colour = newColour;
+    }
+
+    public void BombHexagonTick()
+    {
+        --bombHexagonTimer;
+        text.text = bombHexagonTimer.ToString();
+    }
+
+    public int GettingHexagonX()
+    {
+        return coordinateX;
+    }
+
+    public int GettingHexagonY()
+    {
+        return coordinateY;
+    }
+
+    public int GettingBombhexagonTimer()
+    {
+        return bombHexagonTimer;
+    }
+
+    public Color GettingHexagonColour()
+    {
+        return GetComponent<SpriteRenderer>().color;
     }
 
     void Start()
@@ -49,5 +91,67 @@ public class HexagonTile : ConstantClass
         }
     }
 
-    //burada kaldık devamı yarın yoruldum ya la
+    public void Rotate(int newX, int newY, Vector2 newCoordinates)
+    {
+        linearInterpolationCoordinate = newCoordinates;
+        SettingHexagonX(newX);
+        SettingHexagonY(newY);
+        linearInterpolation = true;
+    }
+
+    public bool IsHexagonRotating()
+    {
+        return linearInterpolation;
+    }
+
+    public bool IsHexagonMoving() // burada bir değişiklik gerekebilir kontrol et! stopped olarak ! olmadan alınabilir mi ?
+    {
+        return !(GetComponent<Rigidbody2D>().velocity == Vector2.zero);
+    }
+
+    public void IsBombHexagonExploded()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    public NeighbouringHexagons GetNeighbouringHexagons()
+    {
+        NeighbouringHexagons neighbouringHexagons;
+        bool onColumn = GridManagerObject.OnColumn(GettingHexagonX());
+
+        neighbouringHexagons.up = new Vector2(coordinateX, coordinateY - 1);
+        neighbouringHexagons.down = new Vector2(coordinateX, coordinateY + 1);
+        neighbouringHexagons.upLeft = new Vector2(coordinateX - 1, onColumn ? coordinateY + 1 : coordinateY);
+        neighbouringHexagons.upRight = new Vector2(coordinateX + 1, onColumn ? coordinateY + 1 : coordinateY);
+        neighbouringHexagons.downLeft = new Vector2(coordinateX - 1, onColumn ? coordinateY : coordinateY - 1);
+        neighbouringHexagons.downRight = new Vector2(coordinateX - 1, onColumn ? coordinateY : coordinateY - 1);
+
+        return neighbouringHexagons;
+    }
+
+    public void AlterWorldPosition(Vector2 newCoordinate)
+    {
+        linearInterpolationCoordinate = newCoordinate;
+        linearInterpolation = true;
+    }
+
+    public void ChangeHexagonGridPosition(Vector2 newCoordinate)
+    {
+        coordinateX = (int)newCoordinate.x;
+        coordinateY = (int)newCoordinate.y;
+    }
+
+    public void SetBombHexagon()
+    {
+        text = new GameObject().AddComponent<TextMesh>();
+        text.alignment = TextAlignment.Center;
+        text.anchor = TextAnchor.MiddleCenter;
+        text.transform.position = new Vector3(transform.position.x, transform.position.y, -4);
+        text.transform.localScale = transform.localScale;
+        text.color = Color.black;
+        text.transform.parent = transform;
+        bombHexagonTimer = _constantBombTimer;
+        text.text = bombHexagonTimer.ToString();
+    }
+
 }
